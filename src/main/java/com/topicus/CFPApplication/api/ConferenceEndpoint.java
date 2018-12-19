9307c889f166f5c5c84a3055571e6c8ac8cb76bc
MakNick <nick_makkinga@hotmail.com>
2018-12-19 12:24:40 +0100

    RestControllers fixed, Tests fixed

--- src/main/java/com/topicus/CFPApplication/api/ConferenceEndpoint.java
+++ src/main/java/com/topicus/CFPApplication/api/ConferenceEndpoint.java
@@ -4,13 +4,6 @@ import java.util.Optional;
 import java.util.Set;
 
 import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/topicus/CFPApplication/persistence/PresentationDraftService.java
+++ src/main/java/com/topicus/CFPApplication/persistence/PresentationDraftService.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import javax.ws.rs.core.Response;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
