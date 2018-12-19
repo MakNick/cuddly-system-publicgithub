4b21228e9b29ebee74b3d6d5b0aee4153d5802a7
MakNick <nick_makkinga@hotmail.com>
2018-12-19 12:04:37 +0100

    Added ConferenceServiceTest

---
+++ src/test/java/com/topicus/CFPApplication/persistence/ConferenceServiceTest.java
@@ -0,0 +1,66 @@
+package com.topicus.CFPApplication.persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.topicus.CFPApplication.domain.Conference;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ConferenceServiceTest {
+
+	@Mock
+	ConferenceRepository conferenceRepo;
+
+	@InjectMocks
+	ConferenceService conferenceService;
+
+	@Test
+	public void findAllTest() {
+		Conference conf = new Conference();
+		List<Conference> list = new ArrayList<>();
+		list.add(conf);
+
+		Mockito.when(this.conferenceRepo.findAll()).thenReturn(list);
+		List<Conference> testList = (List<Conference>) this.conferenceService.findAll();
+
+		Mockito.verify(this.conferenceRepo).findAll();
+
+		Assert.assertEquals(1, testList.size());
+	}
+
+	@Test
+	public void saveTest() {
+		Conference conf = new Conference();
+
+		Mockito.when(this.conferenceRepo.save(conf)).thenReturn(conf);
+
+		Conference testItem = this.conferenceRepo.save(conf);
+
+		Mockito.verify(this.conferenceRepo).save(conf);
+
+		Assert.assertEquals(conf, testItem);
+	}
+
+	@Test
+	public void findByIdTest() {
+		Optional<Conference> opt = Optional.of(new Conference());
+		opt.get().setId(1);
+
+		Mockito.when(this.conferenceRepo.findById((long) 1)).thenReturn(opt);
+
+		Conference testItem = this.conferenceService.findById((long) 1).get();
+
+		Mockito.verify(this.conferenceRepo).findById((long) 1);
+
+		Assert.assertEquals(1, testItem.getId());
+	}
+}
