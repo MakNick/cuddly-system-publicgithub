fb82ff16e4c2563f3d8553a8f2ec8b3b2010552a
Anne-Marije <annemarijeschuurman@gmail.com>
2018-12-27 09:19:36 +0100

    presentationDraftApplicantTest

--- src/test/java/com/topicus/CFPApplication/domain/PresentationDraftApplicantTest.java
+++ src/test/java/com/topicus/CFPApplication/domain/PresentationDraftApplicantTest.java
@@ -5,23 +5,21 @@ import org.junit.Test;
 
 public class PresentationDraftApplicantTest {
 
-	
 	PresentationDraftApplicant presentationDraftApplicant = new PresentationDraftApplicant();
-	
+
 	@Test
-	public void setPresentatoinDraftTest(){
+	public void setPresentatoinDraftTest() {
 		PresentationDraft presentationDraft = new PresentationDraft();
 		presentationDraftApplicant.setPresentationDraft(presentationDraft);
 		Assert.assertEquals(presentationDraft, presentationDraftApplicant.getPresentationDraft());
 	}
-	
+
 	@Test
 	public void addApplicantTest() {
 		Applicant applicant = new Applicant();
 		presentationDraftApplicant.addApplicant(applicant);
-		
+
 		Assert.assertTrue(presentationDraftApplicant.getApplicants().size() == 1);
 	}
-	
-	
+
 }
