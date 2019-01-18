afb75f3af2191c3b1b063effe45a5de554e74505
Kevin Sweeren <sweerenk@gmail.com>
2019-01-18 14:51:28 +0100

    implemented first start of AnneMarije-demo file into project

--- AngularJwtAuth/src/app/aanmeldformulier/aanmeldformulier.component.ts
+++ AngularJwtAuth/src/app/aanmeldformulier/aanmeldformulier.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aanmeldformulier.component.css']
 })
 export class AanmeldformulierComponent implements OnInit {
+  title = 'aanmeldformulier';
 
+    changeTitle($event){
+      this.title = $event.target.value;
+    }
   constructor() { }
 
   ngOnInit() {
