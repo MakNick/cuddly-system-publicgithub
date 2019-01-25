adc8f56eaee487dc256b192986693d5b7b731e94
Kevin Sweeren <sweerenk@gmail.com>
2019-01-25 10:39:56 +0100

    Cohosts aanmelden toegevoegd

--- AngularJwtAuth/src/app/aanmeldformulier/aanmeldformulier.component.ts
+++ AngularJwtAuth/src/app/aanmeldformulier/aanmeldformulier.component.ts
@@ -16,4 +16,44 @@ export class AanmeldformulierComponent implements OnInit {
   ngOnInit() {
   }
 
+  toonMedepresentatoren(event: any){
+    document.getElementById("extra").innerHTML = "";
+
+    for(let i = 0; i < event.target.value ; i++){
+      //naam
+      var naamtekst = document.createTextNode("Naam " + (i+1) + ":");
+      document.getElementById("extra").appendChild(naamtekst);
+      document.getElementById("extra").innerHTML += "<br />";
+
+      var naaminputveld = document.createElement("input");
+      naaminputveld.setAttribute("id", "naam"+event.target.value);
+      naaminputveld.setAttribute("class", "form-control");
+      naaminputveld.setAttribute("required", "required");
+      naaminputveld.setAttribute("oninvalid", "setCustomValidity('Dit veld is verplicht.')");
+      naaminputveld.setAttribute("oninput", "setCustomValidity('')");
+      document.getElementById("extra").appendChild(naaminputveld);
+      document.getElementById("extra").innerHTML += "<br />";
+
+      //e-mail
+      var mailtekst = document.createTextNode("E-mailadres "+ (i+1) + ":");
+      document.getElementById("extra").appendChild(mailtekst);
+      document.getElementById("extra").innerHTML += "<br />";
+
+      var mailinputveld = document.createElement("input");
+      mailinputveld.setAttribute("id", "naam"+event.target.value);
+      mailinputveld.setAttribute("class", "form-control");
+      mailinputveld.setAttribute("required", "required");
+      mailinputveld.setAttribute("oninvalid", "setCustomValidity('Dit veld is verplicht.')");
+      mailinputveld.setAttribute("oninput", "setCustomValidity('')");
+      document.getElementById("extra").appendChild(mailinputveld);
+      document.getElementById("extra").innerHTML += "<br />";
+    }
+
+    if(event.target.value)
+
+    //for loop: maak aantal keren aan: 
+    // label naam, tekstveld naam, label e-mail, tekstveld e-mail
+    // vul div id="extra"
+  }
+
 }
--- AngularJwtAuth/src/app/app.module.ts
+++ AngularJwtAuth/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -36,7 +37,8 @@ import { AanmeldformulierComponent } from './aanmeldformulier/aanmeldformulier.c
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
