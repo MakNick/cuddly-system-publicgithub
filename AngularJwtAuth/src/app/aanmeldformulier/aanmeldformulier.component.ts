a4aead97f4c0820adbce58c601d3f81f438fefc7
Kevin Sweeren <sweerenk@gmail.com>
2019-02-08 11:09:27 +0100

    featureDraftForm voortgang

--- AngularJwtAuth/src/app/aanmeldformulier/aanmeldformulier.component.ts
+++ AngularJwtAuth/src/app/aanmeldformulier/aanmeldformulier.component.ts
@@ -1,96 +1,119 @@
 import { Component, OnInit } from '@angular/core';
-import {ConfigService} from '../config/config.service';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { catchError, map, tap } from 'rxjs/operators';
+import { NgModule } from '@angular/core';
+import { FormBuilder, Validators, FormGroup, EmailValidator, FormArray, FormControl } from '@angular/forms';
+import { Applicant } from '../objects/applicant';
 
 @Component({
   selector: 'app-aanmeldformulier',
   templateUrl: './aanmeldformulier.component.html',
   styleUrls: ['./aanmeldformulier.component.css']
 })
-export class AanmeldformulierComponent implements OnInit {
-  title = 'aanmeldformulier';
+export class AanmeldformulierComponent implements OnInit{
+  public title = 'Aanmeldformulier';
+  public labelnaam = 'naam';
+  public labels = ['Naam','E-mailadres','Onderwerp', 'Omschrijving', 'Type presentatie', 'Categorie', 'Tijdsduur presentatie', 'Beroep/achtergrond', 'Geslacht', 'Overige opmerkingen', 'Wil je mede-presentatoren aanmelden?'];
+  public isZichtbaar = true;
+  public feedbackBerichten = ['Voer a.u.b. een naam in die 3 tekens of langer is.','Voer a.u.b. een geldig e-mailadres in.'];
+  public applicant: Applicant;
+  public numberCohosts: number;
+  public duration: number;
+  public category: String;
+  public geslacht: String;
+
+  presentationdraftForm: FormGroup;
+  //public presentationdraftForm = new FormGroup('');
   
-
-    changeTitle($event){
-      this.title = $event.target.value;
-    }
-  constructor(private http: HttpClient) { }
-
-  ngOnInit() {
-    this.checkAantalCategorieen();
+  ngOnInit(): void {
+    this.numberCohosts = 0;
+    this.presentationdraftForm = new FormGroup({
+      'naam': new FormControl('', [       
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      'email': new FormControl('', [
+        Validators.email                    
+      ]),
+      'naamCohost1': new FormControl('', [       
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      'emailCohost1': new FormControl('', [
+        Validators.email                    
+      ]),
+      'naamCohost2': new FormControl('', [       
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      'emailCohost2': new FormControl('', [
+        Validators.email                    
+      ]),
+      'naamCohost3': new FormControl('', [       
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      'emailCohost3': new FormControl('', [
+        Validators.email                   
+      ])
+    });
   }
 
-  toonMedepresentatoren(event: any){
-    document.getElementById("extra").innerHTML = "";
-
-    for(let i = 0; i < event.target.value ; i++){
-      //naam
-      var naamtekst = document.createTextNode("Naam " + (i+1) + ":");
-      document.getElementById("extra").appendChild(naamtekst);
-      document.getElementById("extra").innerHTML += "<br />";
-
-      var naaminputveld = document.createElement("input");
-      naaminputveld.setAttribute("id", "naam"+event.target.value);
-      naaminputveld.setAttribute("class", "form-control");
-      naaminputveld.setAttribute("required", "required");
-      naaminputveld.setAttribute("oninvalid", "setCustomValidity('Dit veld is verplicht.')");
-      naaminputveld.setAttribute("oninput", "setCustomValidity('')");
-      document.getElementById("extra").appendChild(naaminputveld);
-      document.getElementById("extra").innerHTML += "<br />";
-
-      //e-mail
-      var mailtekst = document.createTextNode("E-mailadres "+ (i+1) + ":");
-      document.getElementById("extra").appendChild(mailtekst);
-      document.getElementById("extra").innerHTML += "<br />";
-
-      var mailinputveld = document.createElement("input");
-      mailinputveld.setAttribute("id", "naam"+event.target.value);
-      mailinputveld.setAttribute("class", "form-control");
-      mailinputveld.setAttribute("required", "required");
-      mailinputveld.setAttribute("oninvalid", "setCustomValidity('Dit veld is verplicht.')");
-      mailinputveld.setAttribute("oninput", "setCustomValidity('')");
-      document.getElementById("extra").appendChild(mailinputveld);
-      document.getElementById("extra").innerHTML += "<br />";
+  checkFormControl(x){
+    if(x.status == 'INVALID'){
+      return true;
+    }else{
+      return false;
     }
   }
 
-  checkAantalCategorieen(){
-    localStorage.setItem("conferenceID","5");
-
-    let conf_id = localStorage.conferenceID;
-    
-    //wat zijn de categorieen bij deze conference?
-    if(conf_id != 0){
-      this.leesCategorieen(conf_id);
-    }
+  get naam(){
+    return this.presentationdraftForm.get('naam');
   }
 
-  leesCategorieen(x: Number){
-    alert("a");
-      // const url = "http://localhost:8082/api/conference/"+x;
-      // let currentList = this.http.get(url);
-      // var categorieen = currentList.categories;
-      // alert(categorieen);
-    
-
-
-
-    
-    // //xhr.onreadystatechange = function(){
-    //   //if(this.readyState == 4 && this.status == 200){
-    //     alert("b");
-    //     let currentList = JSON.parse(this.responseText);
-    //     var categorieen = currentList.categories;
-    //     for(let i=0; i<categorieen.length; i++){
-    //       alert("Cat" + i);
-    //       alert(categorieen[i]);
-    //     //}
-      //}
-    
-    
-    alert(x);
+  get email(){
+    return this.presentationdraftForm.get('email');
   }
 
+  get naamCohost1(){
+    return this.presentationdraftForm.get('naamCohost1');
+  }
+
+  get emailCohost1(){
+    return this.presentationdraftForm.get('emailCohost1');
+  }
+
+  get naamCohost2(){
+    return this.presentationdraftForm.get('naamCohost2');
+  }
+
+  get emailCohost2(){
+    return this.presentationdraftForm.get('emailCohost2');
+  }
+
+  get naamCohost3(){
+    return this.presentationdraftForm.get('naamCohost3');
+  }
+
+  get emailCohost3(){
+    return this.presentationdraftForm.get('emailCohost3');
+  }
+
+  checkNumberCohosts(){
+    return this.numberCohosts;
+  }
+
+  setNumberCohosts(event: any){
+    this.numberCohosts = event.target.value;
+  }
+
+  setDuration(event: any){
+    this.duration = event.target.value;
+  }
+
+  setCategory(event: any){
+    this.category = event.target.value;
+  }
+
+  setGeslacht(event: any){
+    this.geslacht = event.target.value;
+  }
 }
--- AngularJwtAuth/src/app/app.module.ts
+++ AngularJwtAuth/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -38,7 +38,8 @@ import { AanmeldformulierComponent } from './aanmeldformulier/aanmeldformulier.c
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
