2fee5f58bd1c74de1234f5aeafe98570572e4d20
Kevin Sweeren <sweerenk@gmail.com>
2019-02-15 11:58:56 +0100

    Voorstel indienen nu alleen voor user en niet voor admin

--- AngularJwtAuth/src/app/aanmeldformulier/presentationdraftForm/aanmeldformulier.service.ts
+++ AngularJwtAuth/src/app/aanmeldformulier/presentationdraftForm/aanmeldformulier.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { PresentationDraft } from '../../objects/presentation-draft';
 import { PresentationDraftApplicant } from '../../objects/presentationDraftApplicant';
 
 @Injectable({
